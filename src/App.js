0940f48e17f01b8d5637417f62fcd75587ba95bc
Chika <chyngyzalkarbekov@gmail.com>
2022-07-17 20:45:02 +0600

    hw7 save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import './App.css'
 import Comment from './comments/Comment'
-import { comment } from './data/data'
 
 function App() {
+	const comment = [
+		{
+			date: new Date(),
+			text: 'I hope you enjoy learning React!',
+			author: {
+				name: 'Hello Naruto',
+				avatarUrl:
+					'https://play-lh.googleusercontent.com/nCVVCbeSI14qEvNnvvgkkbvfBJximn04qoPRw8GZjC7zeoKxOgEtjqsID_DDtNfkjyo',
+			},
+		},
+		{
+			date: new Date(),
+			text: 'I hope you enjoy learning React!',
+			author: {
+				name: 'Hello Sasuke',
+				avatarUrl:
+					'https://i.pinimg.com/736x/04/d7/a3/04d7a346451b21ad9fb791004188eb7c.jpg',
+			},
+		},
+		{
+			date: new Date(),
+			text: 'I hope you enjoy learning React!',
+			author: {
+				name: 'Hello Sakura',
+				avatarUrl:
+					'https://popartblog.ru/wp-content/uploads/2020/08/sakura-min.png',
+			},
+		},
+	]
+
 	return (
-		<Comment
-			author={comment.author}
-			text={comment.text}
-			date={comment.date}
-		/>
+		<div className='App'>
+			{comment.map((item) => (
+				<Comment
+					author={item.author}
+					text={item.text}
+					date={item.date}
+				/>
+			))}
+		</div>
 	)
 }
 
